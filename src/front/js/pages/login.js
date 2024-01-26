7e5b07abf24b7242cd6ba2f6c114ae37fa2f41d8
Mermate <138124998+Mermate@users.noreply.github.com>
2024-01-26 18:06:13 +0000

    changes: login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Navigate } from "react-router-dom";
 import "../../styles/login.css"
+import Logo from "../../img/Logo200x200.png";
 
 const Login = () => {
 
@@ -11,11 +12,23 @@ const Login = () => {
     const [password, setPassword] = useState('');
     
         return (
-       
-        <div className="text-center">
+           
+            <div className="row w-100">
+               
+            <div className="col-md-6 col-md-12- text-center">
+                <div className="Logo200">
+                <img src={Logo} alt="Logo img" className="Logo200" />
+                </div>
+                </div>
+                
+             
+            <div className="col-md-6 text-center">
+                <div className="Login">
+          
             {store.auth === true ? <Navigate to="/private"/> : null}
             <h1 className="text-center">Login</h1>
-            <form className="w-50 mx-auto" onSubmit={(e)=>actions.loginData(e,email,password)}>
+            <br></br>
+            <form className="w-80 mx-auto" onSubmit={(e)=>actions.loginData(e,email,password)}>
                 <div className="col-auto mb-4">
                     <label htmlFor="staticEmail2" className="visually-hidden">Email</label>
                     <input type="text" onChange={(e)=>setEmail(e.target.value)} className="form-control" id="staticEmail2" placeholder="email@example.com"/>
@@ -25,10 +38,14 @@ const Login = () => {
                     <input type="password" onChange={(e)=>setPassword(e.target.value)} className="form-control" id="inputPassword2" placeholder="Password"/>
                 </div>
                 <div className="col-auto">
-                    <button type="submit" >Enter</button>
+                    <button type="submit " >Enter</button>
                 </div>
             </form>
-        </div>
+             </div>
+             </div>
+             </div>
+             
+    
         
       
     )
