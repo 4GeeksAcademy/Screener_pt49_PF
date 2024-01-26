7abb58fb6a80a07dca84c913491feb3e8f6d1c23
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2024-01-26 16:02:22 +0000

    Cambiando api de movies

--- src/front/js/pages/recomendacion.js
+++ src/front/js/pages/recomendacion.js
@@ -31,16 +31,18 @@ export const Recomendacion = () => {
         .map((movie, index) => (
           <CSSTransition key={index} timeout={500} classNames="fade">
             <div className={index === 0 ? "col-12 text-center" : "col-4 text-center mt-2"}>
-              {index === 0 ? (
-                <>
-                  <h2 className="mb-4">{movie.title}</h2>
-                </>
-              ) : (
-                <h5>{movie.title}</h5>
-              )}
-              <Link to={`/moviedetails/${movie.id}`} key={movie.id} className="ms-1 mt-1">
-                <img style={{ width: index === 0 ? "50%" : "50%" }} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-              </Link>
+              <div className="movie-container">
+                {index === 0 ? (
+                  <>
+                    <h2 className="mb-4">{movie.title}</h2>
+                  </>
+                ) : (
+                  <h5>{movie.title}</h5>
+                )}
+                <Link to={`/moviedetails/${movie.id}`} key={movie.id} className="ms-1 mb-3">
+                  <img style={{ width: index === 0 ? "50%" : "50%" }} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                </Link>
+              </div>
             </div>
           </CSSTransition>
         ))}
@@ -55,11 +57,11 @@ export const Recomendacion = () => {
           {/* Primera pregunta */}
 
           <div className="form-card">
-            <h2 className="fs-title">Con quien vas a ver la pelicula?</h2>
-            <button type="button" onClick={() => setCurrentStep(2)}>Solo</button>
-            <button type="button" onClick={() => setCurrentStep(2)}>Con mi pareja</button>
-            <button type="button" onClick={() => setCurrentStep(2)}>Con niños</button>
-            <button type="button" onClick={() => setCurrentStep(2)}>Con Amigos</button>
+            <h2 className="fs-title text-center mb-3">Con quien vas a ver la pelicula?</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(2)}>Solo</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(18)}>Con mi pareja</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(2)}>Con niños</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(2)}>Con Amigos</button>
 
           </div>
         </>
@@ -72,11 +74,11 @@ export const Recomendacion = () => {
           {/* Solo 1.1 */}
 
           <div className="form-card">
-            <h2 className="fs-title">Que estas buscando hoy?</h2>
-            <button type="button" onClick={() => setCurrentStep(5)}>Necesito algo de fondo para trabajar o limpiar</button>
-            <button type="button" onClick={() => setCurrentStep(3)}>Algo suave, una comedia o de esas light</button>
-            <button type="button" onClick={() => setCurrentStep(9)}>Quiero algo emocionante</button>
-            <button type="button" onClick={() => setCurrentStep(14)}>Venga vamos a llorar un rato</button>
+            <h2 className="fs-title text-center mb-3">Que estas buscando hoy?</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(5)}>Necesito algo de fondo para trabajar o limpiar</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(3)}>Algo suave, una comedia o de esas light</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(9)}>Quiero algo emocionante</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(14)}>Venga vamos a llorar un rato</button>
           </div>
           <input type="button" name="previous" className="previous action-button-previous" onClick={()=>setCurrentStep(1)} value="Previous" />
         </>
@@ -89,10 +91,10 @@ export const Recomendacion = () => {
           {/* Solo 1.2 */}
 
           <div className="form-card">
-            <h2 className="fs-title">Y una cosa más...</h2>
-            <button type="button" onClick={() => setCurrentStep(6)}>De disney o animada</button>
-            <button type="button" onClick={() => setCurrentStep(7)}>Unas buenas risas</button>
-            <button type="button" onClick={() => setCurrentStep(8)}>Una dominguera</button>
+            <h2 className="fs-title text-center mb-3">Y una cosa más...</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(6)}>De disney o animada</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(7)}>Unas buenas risas</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(8)}>Una dominguera</button>
           </div>
           <input type="button" name="previous" className="previous action-button-previous" onClick={()=>setCurrentStep(2)} value="Previous" />
         </>
@@ -105,11 +107,11 @@ export const Recomendacion = () => {
           {/* Solo 1.3 */}
 
           <div className="form-card">
-            <h2 className="fs-title">Emociones fuertes? Dinos un poco más</h2>
-            <button type="button" onClick={() => setCurrentStep(10)}>Algo de accion!</button>
-            <button type="button" onClick={() => setCurrentStep(11)}>Una historica</button>
-            <button type="button" onClick={() => setCurrentStep(12)}>Dame terror ó suspenso</button>
-            <button type="button" onClick={() => setCurrentStep(13)}>De esas que te dejan pensando todo el día</button>
+            <h2 className="fs-title text-center mb-3">Emociones fuertes? Dinos un poco más</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(10)}>Algo de accion!</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(11)}>Una historica</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(12)}>Dame terror ó suspenso</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(13)}>De esas que te dejan pensando todo el día</button>
           </div>
           <input type="button" name="previous" className="previous action-button-previous" onClick={()=>setCurrentStep(2)} value="Previous" />
         </>
@@ -123,12 +125,66 @@ export const Recomendacion = () => {
           {/* Solo 1.4 */}
 
           <div className="form-card">
-            <h2 className="fs-title">A llorar se a dicho</h2>
-            <button type="button" onClick={() => setCurrentStep(15)}>Drama, drama y mas drama</button>
-            <button type="button" onClick={() => setCurrentStep(16)}>Peliculas dificiles de ver</button>
-            <button type="button" onClick={() => setCurrentStep(17)}>Una buena motivadora</button>
+            <h2 className="fs-title text-center mb-3">A llorar se a dicho</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(15)}>Drama, drama y mas drama</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(16)}>Peliculas dificiles de ver</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(17)}>Una buena motivadora</button>
           </div>
           <input type="button" name="previous" className="previous action-button-previous" onClick={()=>setCurrentStep(2)} value="Previous" />
+        </>
+      )
+    },
+
+    {
+      display: currentStep === 18, 
+      content: (
+        <>
+          {/* Primera pregunta */}
+
+          <div className="form-card">
+            <h2 className="fs-title text-center mb-3">Para parejas:</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(19)}>Para crear ambiente</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(23)}>Pelculas emotivas</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(25)}>Domingueras</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(26)}>Novedoso?</button>
+
+          </div>
+          <input type="button" name="previous" className="previous action-button-previous" onClick={()=>setCurrentStep(1)} value="Previous" />
+
+        </>
+      )
+    },
+
+    {
+      display: currentStep === 19, 
+      content: (
+        <>
+          {/* Primera pregunta */}
+
+          <div className="form-card">
+            <h2 className="fs-title text-center mb-3">Perfecto! ahora una cosita más...</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(20)}>Algo romantico</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(21)}>Algo divertido</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(22)}>Algo para hacer maratón</button>
+
+          </div>
+        </>
+      )
+    },
+
+    {
+      display: currentStep === 23, 
+      content: (
+        <>
+          {/* Primera pregunta */}
+
+          <div className="form-card">
+            <h2 className="fs-title text-center mb-3">Perfecto! ahora una cosita más...</h2>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(10)}>Un poco de accion</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(15)}>Mejor algo más sentimental</button>
+            <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(24)}>Algo mas como un thriller</button>
+
+          </div>
         </>
       )
     },
@@ -146,7 +202,7 @@ export const Recomendacion = () => {
             <h2 className="fs-title text-center">La película que no vas a ver:</h2>
             {renderMovies((movie) => movie.white_noise)}
           </div>
-          <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+          <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
         </>
       )
     },
@@ -159,7 +215,7 @@ export const Recomendacion = () => {
             <h2 className="fs-title text-center">Conecta con tu niño interior</h2>
             {renderMovies((movie) => movie.disney || movie.animation && !movie.drama)}
           </div>
-          <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+          <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
         </>
       )
     },
@@ -172,7 +228,7 @@ export const Recomendacion = () => {
             <h2 className="fs-title text-center">Unas risas seguras con:</h2>
             {renderMovies((movie) => movie.comedy && !movie.disney && !movie.animation)}
           </div>
-          <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+          <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
         </>
       )
     },
@@ -185,7 +241,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Domingo de mantita y...</h2>
                 {renderMovies((movie) => movie.comedy || movie.sunday_movie || movie.solitary)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -196,9 +252,9 @@ export const Recomendacion = () => {
             <>
               <div className="form-card">
                 <h2 className="fs-title text-center">Accion? para eso esta:</h2>
-                {renderMovies((movie) => movie.action || movie.violence && !movie.animation)}
+                {renderMovies((movie) => !movie.animation || movie.action || movie.violence )}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -211,7 +267,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Historias reales, gente real:</h2>
                 {renderMovies((movie) => movie.historical)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -224,7 +280,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Viendo películas de terror solo? Pues a ver como te va con:</h2>
                 {renderMovies((movie) => movie.terror || movie.suspence)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -238,7 +294,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Hoy te recomendamos</h2>
                 {renderMovies((movie) => movie.plot_twits)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -251,7 +307,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Vamos a llorar un rato con</h2>
                 {renderMovies((movie) => movie.drama)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -264,7 +320,7 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Una dificil y cruda:</h2>
                 {renderMovies((movie) => movie.hard_to_watch)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -277,7 +333,85 @@ export const Recomendacion = () => {
                 <h2 className="fs-title text-center">Emotiva y motivadora como:</h2>
                 {renderMovies((movie) => movie.motivating)}
               </div>
-              <button type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 20,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">Romaticas:</h2>
+                {renderMovies((movie) => movie.motivating)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 21,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">Alegria y risas:</h2>
+                {renderMovies((movie) => movie.couple && movie.comedy || movie.happy)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 22,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">Para hacer maratón:</h2>
+                {renderMovies((movie) => movie.marathon)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 24,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">Thrillers para parejas:</h2>
+                {renderMovies((movie) => movie.action && movie.violence)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 25,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">Thrillers para parejas:</h2>
+                {renderMovies((movie) => movie.sunday_movie)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
+            </>
+          )
+        },
+
+        {
+          display: currentStep === 26,
+          content: (
+            <>
+              <div className="form-card">
+                <h2 className="fs-title text-center">AGREGAR NUEVA PROPIEDAAAA</h2>
+                {renderMovies((movie) => movie)}
+              </div>
+              <button className="onBoardingButton" type="button" onClick={regenerateRecommendations}>Dame otra Recomendacion</button>
             </>
           )
         },
@@ -293,7 +427,7 @@ export const Recomendacion = () => {
                   <div className="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
                     <div className="card px-0 pt-4 pb-0 mt-3 mb-3">
                       <h2 style={{ color: "black" }}><strong>Recomiendame una pelicula!</strong></h2>
-                      <button type="button" onClick={() => setCurrentStep(1)}>Inicio</button>
+                      <button className="onBoardingButton" type="button" onClick={() => setCurrentStep(1)}>Inicio</button>
                       <div className="row">
                         <div className="col-md-12 mx-0">
                           <form id="msform">
