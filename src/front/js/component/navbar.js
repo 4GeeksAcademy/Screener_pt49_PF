458cc43a3e673a057096a9353be6c5c90724d1a8
Emilio <136965828+Emdihoy@users.noreply.github.com>
2024-01-30 18:05:08 +0000

    Changes: Navbar buttons glow on hover, login and signup have been styled and standardized

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -22,21 +22,21 @@ export const Navbar = () => {
 							<img src={LogoNavbar} alt="Client img" className="rounded" style={{ height: 50, width: 120 }} />
 						</Link>
 					</div>
-					<div className="generalButtons col-lg-8 d-none d-md-block text-start">
-						<Link to={"/moviesUser"} ><button >Peliculas</button></Link>
-						<Link to={"/randomMovie"} ><button >Pelicula aleatoria</button></Link>
+					<div className="col-lg-8 d-none d-md-block text-start">
+						<Link to={"/moviesUser"} ><button className="navButton" >Peliculas</button></Link>
+						<Link to={"/randomMovie"} ><button className="navButton" >Pelicula aleatoria</button></Link>
 						<Link to={"/watchlistUser"} >
-							{store.auth === true ? <button className="btn text-light">Peliculas guardadas<span className="badge bg-secondary rounded-pill ms-2">{store.watchlist.length}</span>
+							{store.auth === true ? <button className="navButton btn text-light">Peliculas guardadas<span className="badge bg-secondary rounded-pill ms-2">{store.watchlist.length}</span>
 							</button> : null}
 						</Link>
 					</div>
 					<div className="logButton btn-group col-lg-3 col-sm-5 col-md-4">
 						<div className="">
-							{store.auth === true ? <Link to={"/private"} ><button >Perfil</button></Link> : null}
-							{store.adminLogin === true ? <button onClick={() => actions.adminLogOut()} >ADMIN Logout</button> : null}
-							{store.auth === true ? <Link to="/"><button onClick={() => actions.logOut()} >Cerrar sesion</button></Link> :
+							{store.auth === true ? <Link to={"/private"} ><button className="navButton">Perfil</button></Link> : null}
+							{store.adminLogin === true ? <button className="navButton" onClick={() => actions.adminLogOut()} >ADMIN Logout</button> : null}
+							{store.auth === true ? <Link to="/"><button className="navButton" onClick={() => actions.logOut()} >Cerrar sesion</button></Link> :
 								<div className="btn-group" role="group" aria-label="Basic example">
-									<Link to="/login"><button type="button" >Iniciar sesion</button></Link>
+									<Link to="/login"><button className="navButton" type="button" >Iniciar sesion</button></Link>
 								</div>
 							}
 						</div>
