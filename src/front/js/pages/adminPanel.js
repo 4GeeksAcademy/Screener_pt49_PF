30173b8b8e01d4a59242f2d15f1c40362e01b3dd
Emilio <136965828+Emdihoy@users.noreply.github.com>
2024-01-30 08:27:09 +0000

    Changes: general buttons and adminPanel have been fixed

--- src/front/js/pages/adminPanel.js
+++ src/front/js/pages/adminPanel.js
@@ -5,40 +5,35 @@ import { Navigate } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/adminPanel.css";
 import { UserForm } from "./usersForm";
-import  Login  from "./login";
+import Login from "./login";
 
 // import  Navbar_admin  from "./component/navbar_admin";
 export const AdminPanel = () => {
-    const { store, actions } = useContext(Context);
-    return (
-        <div className="text-center mt-5">
-        {store.adminLogin === false ? <Navigate to="/adminLogin" /> : null}
-      
-        <div className="btn-row">
-          <Link to={"/movies"}>
-            <button className="btn btn-light btn-lg">Crear una película de la lista</button>
-          </Link>
-          <Link to={"/movieByName"}>
-            <button className="btn btn-light btn-lg">Crear una película por nombre</button>
-          </Link>
-          <Link to={"/moviesApi"}>
-            <button className="btn btn-light btn-lg">Administrador de peliculas</button>
-          </Link>
-        </div>
-      
-        <div className="btn-row">
-          <Link to="/users">
-            <button className="btn btn-light btn-lg">Administrador de usuarios</button>
-          </Link>
-          <Link to={"/movies/comment_list"}>
-            <button className="btn btn-light btn-lg">Administrador de comentarios</button>
-          </Link>
-          <Link to={"/watchlist"}>
-            <button className="btn btn-light btn-lg">Watchlist</button>
-          </Link>
-        </div>
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="adminButtonsContainer mt-5">
+      {store.adminLogin === false ? <Navigate to="/adminLogin" /> : null}
+      <div className="btn-row">
+        <Link to={"/movies"}>
+          <button className="adminButtons btn btn-light btn-lg">Crear una película de la lista</button>
+        </Link>
+        <Link to={"/movieByName"}>
+          <button className="adminButtons btn btn-light btn-lg">Crear una película por nombre</button>
+        </Link>
+        <Link to={"/moviesApi"}>
+          <button className="adminButtons btn btn-light btn-lg">Administrador de peliculas</button>
+        </Link>
+        <Link to="/users">
+          <button className="adminButtons btn btn-light btn-lg">Administrador de usuarios</button>
+        </Link>
+        <Link to={"/movies/comment_list"}>
+          <button className="adminButtons btn btn-light btn-lg">Administrador de comentarios</button>
+        </Link>
+        <Link to={"/watchlist"}>
+          <button className="adminButtons btn btn-light btn-lg">Watchlist</button>
+        </Link>
       </div>
-      
-    );
+    </div>
+  );
 };
 
